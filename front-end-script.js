d10ddb605a1de6d1086120ba4eb61216a44f42c5
Daria <dariagnaumova@gmail.com>
2020-01-11 13:57:44 -0500

    onclick added

---
+++ front-end-script.js
@@ -0,0 +1,18 @@
+var array = [];
+
+
+$("#search").click(function(){
+
+var searchInp = $("#searchTerm").val().trim();
+var recNum = $("#FormControlOptions").val();
+var startYearInp = $("#startYear").val().trim();
+var endYearInp = $("#endYear").val().trim();
+
+array = [searchInp, recNum, startYearInp, endYearInp];
+
+console.log(array);
+return (gussfunction);
+
+
+});
+
