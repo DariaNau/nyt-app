92474d3f72af467e8a1172c2744f64fc8cd651eb
John Sasser <cw.john.sasser@gmail.com>
2020-01-11 14:27:37 -0500

    appending

--- front-end-script.js
+++ front-end-script.js
@@ -1,20 +1,44 @@
 var array = [];
+var $listItemReturn = $(".list-group").create("<div>");
+console.log();
+$("#search").click(function() {
+	var searchInp = $("#searchTerm")
+		.val()
+		.trim();
+	var recNum = $("#FormControlOptions").val();
+	var startYearInp = $("#startYear")
+		.val()
+		.trim();
+	var endYearInp = $("#endYear")
+		.val()
+		.trim();
 
-$('#search').click(function() {
-	var searchInp = $('#searchTerm').val().trim();
-	var recNum = $('#FormControlOptions').val();
-	var startYearInp = $('#startYear').val().trim();
-	var endYearInp = $('#endYear').val().trim();
+	$("#search").click(function() {
+		var searchInp = $("#searchTerm")
+			.val()
+			.trim();
+		var recNum = $("#FormControlOptions").val();
+		var startYearInp = $("#startYear")
+			.val()
+			.trim();
+		var endYearInp = $("#endYear")
+			.val()
+			.trim();
 
-	$('#search').click(function() {
-		var searchInp = $('#searchTerm').val().trim();
-		var recNum = $('#FormControlOptions').val();
-		var startYearInp = $('#startYear').val().trim();
-		var endYearInp = $('#endYear').val().trim();
-
-		array = [ searchInp, recNum, startYearInp, endYearInp ];
+		array = [searchInp, recNum, startYearInp, endYearInp];
 
 		console.log(array);
 		return request();
 	});
 });
+function appendAJAXres(object) {
+	var newDiv = $("<div>");
+
+	$listItemReturn.append(title);
+	$listItemReturn.append(url + of + artile);
+	$listItemReturn.append(article);
+
+	// two vars
+	// - url array
+	// - title array
+}
