241f5c85aca3be99318695cc091f2f66a6c77d35
GusGo <gus.gonzalez.15@gmail.com>
2020-01-11 14:06:22 -0500

    update

--- front-end-script.js
+++ front-end-script.js
@@ -6,18 +6,15 @@ $('#search').click(function() {
 	var startYearInp = $('#startYear').val().trim();
 	var endYearInp = $('#endYear').val().trim();
 
-$("#search").click(function(){
+	$('#search').click(function() {
+		var searchInp = $('#searchTerm').val().trim();
+		var recNum = $('#FormControlOptions').val();
+		var startYearInp = $('#startYear').val().trim();
+		var endYearInp = $('#endYear').val().trim();
 
-var searchInp = $("#searchTerm").val().trim();
-var recNum = $("#FormControlOptions").val();
-var startYearInp = $("#startYear").val().trim();
-var endYearInp = $("#endYear").val().trim();
+		array = [ searchInp, recNum, startYearInp, endYearInp ];
 
-array = [searchInp, recNum, startYearInp, endYearInp];
-
-console.log(array);
-return (request());
-
-	console.log(array);
-	return gussfunction;
+		console.log(array);
+		return request();
+	});
 });
